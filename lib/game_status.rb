d7d42df9896eb2037118b54d8ccb956423101a4c
Steve C <stephen.cases@gmail.com>
2020-03-10 18:51:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,6 +8,8 @@ WIN_COMBINATIONS = [
   [0,1,2], [3,4,5], [6,7,8], [0,4,8], [0,3,6], [1,4,7], [2,4,6], [2,5,8]
 ]
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def won?(board)
  WIN_COMBINATIONS.each do |win_combination|
    win_index_1 = win_combination[0]
